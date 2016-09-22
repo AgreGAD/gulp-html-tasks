a1552d1cd60fd016a35c6d60a1bb132e32ac1d76
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-09-22 08:58:43 +0700

    First functionality

---
+++ index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+var
+    gulpWebpackTask = require('gulp-webpack-task'),
+    watch = require('gulp-watch'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    pugBlockCompiler = require('./pugBlockCompiler');
+
+var defaultConfig = {
+    entry: 'blocks/layout/layout.js',
+    blockPath: 'blocks/*/*.pug',
+    pugWatchList: ['blocks/*/*.pug', 'blocks/*/data.js'],
+    destination: 'var',
+    extract: true
+};
+
+var getCleanTask = function (config) {
+    return function () {
+        return del(config.destination);
+    };
+};
+
+var getAssetsTask = function (gulp, config, aliases, provides, isDevelopment) {
+    return gulpWebpackTask(gulp, {
+        entry: config.entry,
+        destination: config.destination + '/',
+        aliases: aliases,
+        provides: provides,
+        watch: isDevelopment,
+        extract: config.extract
+    });
+};
+
+var getServeTask = function (config) {
+    return function () {
+        browserSync.init({
+            server: config.destination + '/'
+        });
+
+        browserSync.watch(config.destination + '/**/*.*').on('change', browserSync.reload);
+    }
+};
+
+var getPugTask = function (gulp, config) {
+    // todo: mystic destination path
+    return function () {
+        return gulp.src(config.blockPath)
+            .pipe(pugBlockCompiler())
+            .pipe(gulp.dest(config.destination + '/test/'));
+    };
+};
+
+var getPugWatchTask = function (gulp, config) {
+    return function () {
+        watch(config.pugWatchList, gulp.series('pug'));
+    };
+};
+
+var getDefaultTask = function (gulp, isDevelopment) {
+    return isDevelopment
+        ? gulp.series('clean', 'pug', gulp.parallel('assets', 'pug_watch', 'serve'))
+        : gulp.series('clean', 'pug', 'assets');
+};
+
+module.exports = function (gulp, config) {
+    gulp.task('clean', getCleanTask(defaultConfig));
+    gulp.task('assets', getAssetsTask(gulp, defaultConfig, config.aliases, config.provides, config.isDevelopment));
+    gulp.task('serve', getServeTask(defaultConfig));
+    gulp.task('pug', getPugTask(gulp, defaultConfig));
+    gulp.task('pug_watch', getPugWatchTask(gulp, defaultConfig));
+    gulp.task('default', getDefaultTask(gulp, config.isDevelopment));
+};
---
+++ pugBlockCompiler.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var
+    _ = require('lodash'),
+    through = require('through2'),
+    path = require('path'),
+    fs = require('fs'),
+    pug = require('pug'),
+    ext = require('gulp-util').replaceExtension;
+
+var loadData = function (filepath) {
+    var data = undefined;
+
+    try {
+        fs.accessSync(filepath, fs.F_OK);
+
+        delete require.cache[require.resolve(filepath)];
+        data = require(filepath);
+    } catch (e) {
+        data = {};
+    }
+
+    return data;
+};
+
+var calcBlockName = function (filename) {
+    var words = _.split(filename, '-');
+    var upperWords = _.map(words, function (word) {
+        return _.upperFirst(word);
+    });
+
+    return _.lowerFirst(_.join(upperWords, ''));
+};
+
+var getBlockTemplate = function (filename) {
+    var blockName = calcBlockName(filename);
+
+    return `
+include ./${filename}/${filename}.pug
+include ./layout/layout.pug
+
++layout(layoutData)
+  +${blockName}(blockData)
+    `;
+};
+var getLayoutTemplate = function () {
+    return `
+include ./layout/layout.pug
+
++layout(layoutData)
+    `;
+};
+
+var pugCompile = function (template, data, tmpFile) {
+    var fn = pug.compile(template, {
+        pretty: true,
+        filename: tmpFile
+    });
+
+    return fn(data);
+};
+
+var compile = function (file, enc, cb) {
+
+    console.log(file.path);
+
+
+
+    var dirname = path.dirname(file.path);
+    var blocksPath = dirname + '/..';
+
+    file.path = file.cwd + '/' + path.basename(file.path);
+
+    var templateFilename = path.basename(file.path, '.pug');
+    var template = (templateFilename == 'layout') ? getLayoutTemplate() : getBlockTemplate(templateFilename);
+
+    var html = pugCompile(template, {
+        layoutData: loadData(blocksPath + '/layout/data.js'),
+        blockData: loadData(dirname + '/data.js')
+    }, dirname + '/../tmp.pug');
+
+    file.path = ext(file.path, '.html');
+
+    console.log(file.path);
+
+    if (file.isBuffer()) {
+        file.contents = new Buffer(html);
+    }
+
+    cb(null, file);
+};
+
+module.exports = function () {
+    return through.obj(compile);
+};
